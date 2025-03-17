ada110f035d023faaae34f3691ed399b3a852d66
JiangY03 <120696912+JiangY03@users.noreply.github.com>
2025-03-17 13:09:12 +0000

    Delete market directory

--- market/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('market/', views.market, name='market'),
-    path('buy_skin/<int:item_id>/', views.buy_skin, name='buy_skin'),
-]
-
