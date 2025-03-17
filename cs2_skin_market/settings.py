150fc1c29351b357beb991af357fa2a69c7ef454
JiangY03 <120696912+JiangY03@users.noreply.github.com>
2025-03-17 11:01:29 +0000

    Update settings.py

--- cs2_skin_market/settings.py
+++ cs2_skin_market/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ITGroup96.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Installed apps
 INSTALLED_APPS = [
